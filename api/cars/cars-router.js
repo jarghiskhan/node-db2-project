dc6637bb24f65d28c4a3b06c999da568ca39347c
jarghiskhan <jared.l.wyatt@outlook.com>
2021-10-16 16:28:16 -0400

    completed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,42 @@
 // DO YOUR MAGIC
+const express = require("express");
+const Cars = require("../cars/cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("../cars/cars-middleware");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  // DO YOUR MAGIC
+  Cars.getAll()
+    .then((account) => {
+      res.status(200).json(account);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "unable to get data" });
+    });
+});
+router.get("/:id", checkCarId, (req, res, next) => {
+  const { id } = req.params;
+  // DO YOUR MAGIC
+  Cars.getById(id).then((cars) => {
+    res.status(200).json(cars);
+  });
+});
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, (req, res, next) => {
+    const newCar = req.body;
+    // DO YOUR MAGIC
+    Cars.create(newCar)
+    .then((car)=>{
+      res.status(201).json(car)
+    })
+    .catch((err)=>{
+        console.log(err)
+      res.status(500).json({message:"unable to add that car"})
+    })
+  })
+
+module.exports = router;
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl=>{
+    tbl.increments();
+    tbl.string("vin",20).unique().notNullable();
+    tbl.string("make", 20).notNullable();
+    tbl.string("model", 20).notNullable();
+    tbl.integer("mileage").notNullable();
+    tbl.string("title", 128)
+    tbl.string("transmission",20)
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars")
 };
